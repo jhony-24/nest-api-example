1501fb5c39e406df7794b2b0edc0177774d7a739
jhony-24 <jhonyvegacuya24@gmail.com>
2021-03-13 17:01:51 -0500

    feat: Create services and controllers in module folder

--- src/modules/account/account.module.ts
+++ src/modules/account/account.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AccountService } from './account.service';
+import { AccountController } from './account.controller';
 
-@Module({})
+@Module({
+  providers: [AccountService],
+  controllers: [AccountController]
+})
 export class AccountModule {}
--- src/modules/posts/posts.module.ts
+++ src/modules/posts/posts.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Posts, PostsSchema } from 'src/core/schemas/Posts.schema';
+import { PostsController } from './posts.controller';
+import { PostsService } from './posts.service';
 
-@Module({})
+@Module({
+  providers: [
+    MongooseModule.forFeature([{ name: Posts.name, schema: PostsSchema }]),
+    PostsService,
+  ],
+  controllers: [PostsController],
+})
 export class PostsModule {}
