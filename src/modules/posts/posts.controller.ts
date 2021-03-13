2a4de8748899a3b7001dc1e8f41a13fa6c01b02f
jhony-24 <jhonyvegacuya24@gmail.com>
2021-03-13 17:37:59 -0500

    feat: Add endpoints to post controller

--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import ICreateNewPost from './interfaces/ICreateNewPost';
+import { PostsService } from './posts.service';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  async getAllPosts(): Promise<string[]> {
+    return [];
+  }
+
+  @Get(':id')
+  async getPostById(@Param('id') id: string): Promise<string> {
+    return `Post from ${id}`;
+  }
+
+  @Post()
+  async createNewPost(@Body() post: ICreateNewPost): Promise<string> {
+    console.log(post);
+    return '';
+  }
+}
