375e617dbaa827492b0b06d4fee48e9c2be7584c
jhony-24 <jhonyvegacuya24@gmail.com>
2021-03-13 17:50:48 -0500

    feat: Create posts service

--- src/modules/posts/posts.service.ts
+++ src/modules/posts/posts.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Posts, PostsDocument } from 'src/core/schemas/Posts.schema';
+import { Model } from 'mongoose';
+import ICreateNewPost from './interfaces/ICreateNewPost';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(
+    @InjectModel(Posts.name) private readonly postsModel: Model<PostsDocument>,
+  ) {}
+
+  async getAllPosts(): Promise<Posts[]> {
+    return this.postsModel.find().exec();
+  }
+
+  async getPostById(id: string): Promise<Posts> {
+    return this.postsModel.findOne({ id }).exec();
+  }
+
+  async createNewPost(newPost: ICreateNewPost): Promise<Posts> {
+    const createdPost = await this.postsModel.create(newPost);
+    return createdPost.save();
+  }
+}
