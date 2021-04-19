e8effd711866c0e3c28f89a4586769b95683c03b
jhony-24 <jhonyvegacuya24@gmail.com>
2021-04-19 07:55:29 -0500

    Add parking module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PostsModule } from './modules/posts/posts.module';
 import { AccountModule } from './modules/account/account.module';
+import { ParkingModule } from './modules/parking/parking.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MONGO_URL } from './config';
 
 @Module({
-  imports: [MongooseModule.forRoot(MONGO_URL), PostsModule, AccountModule],
+  imports: [
+    MongooseModule.forRoot(MONGO_URL),
+    PostsModule,
+    AccountModule,
+    ParkingModule,
+  ],
 })
 export class AppModule {}
